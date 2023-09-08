3886c2c8e3daef1610e92f23678cd4d13e676dd5
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-08 17:36:01 +0330

    Rendering the Root Component and StrictMode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
+// React v18
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// React before 18
+// import ReactDOM from 'react-dom';
 
+function App() {
+    return <h1>Hello React</h1>;
+}
+
+// React v18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    React.createElement(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    )
 );
+
+// React before 18
+// React.render(<App/>);
