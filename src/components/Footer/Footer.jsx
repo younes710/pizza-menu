fc6d25ba3c96e169ab20b2e0a71906646d5dc229
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-08 20:25:44 +0330

    Add Footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles['footer']}>
             {new Date().toLocaleTimeString()}
             We're currently open
         </footer>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 // import ReactDOM from 'react-dom';
 import styles from './index.css';
 import Header from './components/Header/Header';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import Menu from './components/Menu';
 
 const pizzaData = [
