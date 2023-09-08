5ab2fcd62580b0b0c0daaef8528ef39521fd7f78
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-08 18:33:45 +0330

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Fast React Pizza Co.</h1>
+        </div>
+    );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,69 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 // React v18
 import ReactDOM from 'react-dom/client';
 // React before 18
 // import ReactDOM from 'react-dom';
+import Header from './components/Header';
 
-function App() {
-    return <h1>Hello React</h1>;
-}
+const pizzaData = [
+    {
+        name: 'Focaccia',
+        ingredients: 'Bread with italian olive oil and rosemary',
+        price: 6,
+        photoName: 'pizzas/focaccia.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Margherita',
+        ingredients: 'Tomato and mozarella',
+        price: 10,
+        photoName: 'pizzas/margherita.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Spinaci',
+        ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+        price: 12,
+        photoName: 'pizzas/spinaci.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Funghi',
+        ingredients: 'Tomato, mozarella, mushrooms, and onion',
+        price: 12,
+        photoName: 'pizzas/funghi.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Salamino',
+        ingredients: 'Tomato, mozarella, and pepperoni',
+        price: 15,
+        photoName: 'pizzas/salamino.jpg',
+        soldOut: true,
+    },
+    {
+        name: 'Pizza Prosciutto',
+        ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+        price: 18,
+        photoName: 'pizzas/prosciutto.jpg',
+        soldOut: false,
+    },
+];
 
 // React v18
+function App() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    React.createElement(
-        <StrictMode>
-            <App />
-        </StrictMode>
-    )
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // React before 18
