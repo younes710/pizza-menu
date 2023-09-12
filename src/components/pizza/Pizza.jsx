522ffbaff549ec7155cbbfaf9bbe40eda2bab125
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-12 12:36:06 +0330

    Setting classes and text conditionally

--- src/components/pizza/Pizza.jsx
+++ src/components/pizza/Pizza.jsx
@@ -2,12 +2,16 @@ import styles from './pizza.module.css';
 
 const Pizza = ({ pizzaObj }) => {
     return (
-        <li className={styles['pizza']}>
+        <li
+            className={`${styles['pizza']} ${
+                pizzaObj.soldOut ? styles['sold-out'] : ''
+            }`}
+        >
             <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
                 <h3>{pizzaObj.name}</h3>
                 <p>{pizzaObj.ingredients}</p>
-                <span>{pizzaObj.price}</span>
+                <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
             </div>
         </li>
     );
